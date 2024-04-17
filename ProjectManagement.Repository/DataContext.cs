5cca51bf65d82ed24925d52044e34b44d4e20f69
pankaj7464 <pkumar44676@gmail.com>
2024-04-17 22:07:55 +0530

    Add project files.

---
+++ ProjectManagement.Repository/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using ProjectManagement.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.Repository
+{
+    public class DataContext : IdentityDbContext<IdentityUser>
+    {
+
+    }
+}
